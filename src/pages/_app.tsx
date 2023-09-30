1c3614fb4be5c2640e19f95584a7aed7d7e8b038
Aaron Cohen <aaronroicohen@gmail.com>
2023-09-30 11:40:25 +0100

    added vercel analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,9 @@
+// src/pages/_app.tsx
 import 'styles/globals.css'
 import 'styles/tailwind.css'
 import React from 'react';
-
+import { Analytics } from '@vercel/analytics/react';
 import { ModalProvider, ToastProvider } from '@apideck/components'
-
 import { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps): JSX.Element {
@@ -11,6 +11,7 @@ export default function App({ Component, pageProps }: AppProps): JSX.Element {
     <ToastProvider>
       <ModalProvider>
         <Component {...pageProps} />
+        <Analytics />
       </ModalProvider>
     </ToastProvider>
   )
