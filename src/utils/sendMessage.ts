d697b224ba5934587d6549a8830bbb22410ceda2
Aaron Cohen <aaronroicohen@gmail.com>
2023-10-05 01:40:33 +0100

    removed superfluous code in sendMessage to request step by step. removed old scripts

--- src/utils/sendMessage.ts
+++ src/utils/sendMessage.ts
@@ -2,12 +2,6 @@ import OpenAI from 'openai'
 
 export const sendMessage = async (messages: OpenAI.Chat.CreateChatCompletionRequestMessage[]) => {
   try {
-    // Check if the last user message is a request for a step-by-step explanation
-    const lastUserMessage = messages[messages.length - 1]
-    if (lastUserMessage.role === 'user' && lastUserMessage.content.toLowerCase().includes('step-by-step explanation')) {
-      // Modify the user's message to include a specific prompt for a step-by-step explanation
-      lastUserMessage.content = `Please provide a step-by-step explanation for the following: ${lastUserMessage.content.replace('step-by-step explanation', '')}`
-    }
 
     const response = await fetch('/api/createMessage', {
       method: 'POST',
