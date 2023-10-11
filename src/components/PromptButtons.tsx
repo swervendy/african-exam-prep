989b24a1ce94e0775307cbe880ef6931fe04c4ce
Aaron Cohen <aaronroicohen@gmail.com>
2023-10-11 14:35:43 +0300

    started split of step by step

--- src/components/PromptButtons.tsx
+++ src/components/PromptButtons.tsx
@@ -2,16 +2,14 @@ import React from 'react';
 
 type Props = {
   onPromptClick: (prompt: string) => void;
-  mode: string;
 };
 
-const prompts = ['Step-by-step explanation'];
-const stepByStepPrompt = 'Next step';
+const prompts = ['Chat with tutor'];
 
-const PromptButtons: React.FC<Props> = ({ onPromptClick, mode }) => {
+const PromptButtons: React.FC<Props> = ({ onPromptClick }) => {
   return (
     <div className="flex space-x-4 px-4 sm:px-0">
-      {mode === 'normal' && prompts.map((prompt, index) => (
+      {prompts.map((prompt, index) => (
         <button
           key={index}
           className="border-2 border-indigo-500 hover:bg-indigo-500 text-indigo-500 hover:text-white font-normal py-2 px-2 sm:px-4 rounded w-full sm:w-auto"
@@ -20,14 +18,6 @@ const PromptButtons: React.FC<Props> = ({ onPromptClick, mode }) => {
           {prompt}
         </button>
       ))}
-      {mode === 'step-by-step' && (
-        <button
-          className="border-2 border-indigo-500 hover:bg-indigo-500 text-indigo-500 hover:text-white font-normal py-2 px-2 sm:px-4 rounded w-full sm:w-auto"
-          onClick={() => onPromptClick(stepByStepPrompt)}
-        >
-          {stepByStepPrompt}
-        </button>
-      )}
     </div>
   );
 };
