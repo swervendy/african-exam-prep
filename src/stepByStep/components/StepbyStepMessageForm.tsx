03ce6dfd639b69324b43d7538e23fb2e7fbe4908
Aaron Cohen <aaronroicohen@gmail.com>
2023-10-11 16:14:06 +0300

    added final portion to get different explanation

--- src/stepByStep/components/StepbyStepMessageForm.tsx
+++ src/stepByStep/components/StepbyStepMessageForm.tsx
@@ -4,13 +4,20 @@ import React, { useState } from 'react';
 
 const MessageForm: React.FC = () => {
   const { addMessage, remainingMessages, setRemainingMessages } = useMessages()
-  const [explanationStarted, setExplanationStarted] = useState(false);
+  const [explanationStarted, setExplanationStarted] = useState<boolean | string>(false);
 
   const handleStartExplanation = async () => {
     if (explanationStarted && remainingMessages.length > 0) {
       // If the explanation has already started and there are remaining messages, add the next message to the list
       addMessage(remainingMessages[0], 'assistant', 'assistant');
       setRemainingMessages(remainingMessages.slice(1));
+      if (remainingMessages.length === 1) {
+        setExplanationStarted('final');
+      }
+    } else if (explanationStarted === 'final') {
+      // If the explanation is final, retrigger the response from OpenAI
+      addMessage("Start explanation", 'user', 'user') // 'user' is passed as the role and sender
+      setExplanationStarted(true);
     } else {
       // If the explanation hasn't started yet, start it
       addMessage("Start explanation", 'user', 'user') // 'user' is passed as the role and sender
@@ -18,10 +25,12 @@ const MessageForm: React.FC = () => {
     }
   }
 
+  const buttonText = explanationStarted === 'final' ? 'Get another explanation' : explanationStarted ? 'Next step' : 'Start explanation';
+
   return (
     <div className="flex justify-center items-center h-screen pb-">
       <Button className="inline-flex items-center justify-center border border-transparent leading-4 font-medium rounded transition duration-300 ease-in-out whitespace-nowrap bg-indigo-500 text-white shadow hover:shadow-md active:bg-indigo-600 hover:bg-indigo-700 focus:shadow-outline-indigo dark:hover:bg-indigo-500 px-6 py-3 text-lg" onClick={handleStartExplanation}>
-        {explanationStarted ? "Next step" : "Start explanation"}
+        {buttonText}
       </Button>
     </div>
   )
