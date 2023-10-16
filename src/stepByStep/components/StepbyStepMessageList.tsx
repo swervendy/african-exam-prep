e16f31d996a41843faff966e057e700a658fefa7
Aaron Cohen <aaronroicohen@gmail.com>
2023-10-16 17:48:14 +0300

    html5 audio player to replace previous method so iOS works

--- src/stepByStep/components/StepbyStepMessageList.tsx
+++ src/stepByStep/components/StepbyStepMessageList.tsx
@@ -12,6 +12,7 @@ const MessagesList = () => {
   useEffect(scrollToBottom, [messages]);
 
   const [isGeneratingAudio, setIsGeneratingAudio] = useState(false);
+  const [audioUrls, setAudioUrls] = useState<{ [key: string]: string | null }>({});
 
   const generateAudio = async (message: string) => {
     setIsGeneratingAudio(true);
@@ -60,113 +61,113 @@ const MessagesList = () => {
 
   return (
     <div className="max-w-3xl mx-auto pt-8 px-4">
-     {messages?.map((message, i) => {
-        const isUser = message.role === 'user'
-        if (message.role === 'system') return null
+    {messages?.map((message, i) => {
+      const isUser = message.role === 'user'
+      if (message.role === 'system') return null
 
-        const handlePlayAudio = async () => {
-          console.log('Generating audio for:', message.content);
-          
-          // Generate the audio
-          const audioUrl = await generateAudio(message.content);
-          
-          // Log the audio URL
-          console.log('Audio URL:', audioUrl);
-          
-          // Play the audio
-          const audio = new Audio(audioUrl);
-          audio.oncanplaythrough = () => {
-            console.log('Audio can play through.');
-            audio.play().catch((error) => {
-              console.error('Error occurred when trying to play:', error);
-            });
-          }
-          audio.onerror = (error) => {
-            console.error('Error occurred when loading the audio:', error);
-          }
-        }
-        
-        return (
+      const audioUrl = audioUrls[message.content];
+
+      const handleGenerateAudio = async () => {
+        console.log('Generating audio for:', message.content);
+        // Generate the audio
+        const generatedAudioUrl = await generateAudio(message.content);
+        // Set the audioUrl state
+        setAudioUrls(prev => ({ ...prev, [message.content]: generatedAudioUrl }));
+        // Log the audio URL
+        console.log('Audio URL:', generatedAudioUrl);
+      
+        // Play the audio
+        const audio = new Audio(generatedAudioUrl);
+        audio.play();
+      }
+      return (
+        <div
+          id={`message-${i}`}
+          className={`flex mb-4 fade-up ${isUser ? 'justify-end' : 'justify-start'} ${
+            i === 1 ? 'max-w-md' : ''
+          }`}
+          key={message.content}
+          style={{ position: 'relative' }} // Add this line
+        >
+          {!isUser && (
+            <img
+              src="/img/tutor-avatar.png" 
+              className="w-9 h-9 rounded-full"
+              alt="avatar"
+            />
+          )}
           <div
-            id={`message-${i}`}
-            className={`flex mb-4 fade-up ${isUser ? 'justify-end' : 'justify-start'} ${
-              i === 1 ? 'max-w-md' : ''
+            style={{ maxWidth: 'calc(100% - 45px)' }}
+            className={`group relative px-3 py-2 rounded-lg flex flex-col justify-between text-right ${
+              isUser
+                ? 'ml-2 bg-indigo-500 dark:bg-indigo-300 text-white dark:text-black'
+                : 'ml-2 bg-gray-200 dark:bg-gray-800 text-gray-700 dark:text-gray-200'
             }`}
-            key={message.content}
-            style={{ position: 'relative' }} // Add this line
           >
-            {!isUser && (
-              <img
-                src="/img/tutor-avatar.png" 
-                className="w-9 h-9 rounded-full"
-                alt="avatar"
-              />
-            )}
-            <div
-              style={{ maxWidth: 'calc(100% - 45px)' }}
-              className={`group relative px-3 py-2 rounded-lg flex flex-col justify-between text-right ${
-                isUser
-                  ? 'ml-2 bg-indigo-500 dark:bg-indigo-300 text-white dark:text-black'
-                  : 'ml-2 bg-gray-200 dark:bg-gray-800 text-gray-700 dark:text-gray-200'
-              }`}
-            >
-              <div className="text-left">
-                {message.content.split('. ').map((part, index) => {
-                  if (part.startsWith('The correct answer was:')) {
-                    return (
-                      <>
-                        <p key={index} className="font-bold">{part}</p>
-                        <p key={`${index}-break`}>&nbsp;</p> {/* This adds a line break */}
-                      </>
-                    )
-                  } else if (part.startsWith('Your answer was:')) {
-                    return (
-                      <>
-                        <p key={index}>{part}</p>
-                        <p key={`${index}-break`}>&nbsp;</p> {/* This adds a line break */}
-                      </>
-                    )
-                  } else {
-                    return <p key={index}>{part}</p>
-                  }
-                })}
-              </div>
-              {!isUser && (
-                <button 
-                onClick={handlePlayAudio} 
-                className="self-end mt-2 bg-00 text-white font-bold py-2 px-4 rounded shadow active:shadow-none"
-                disabled={isGeneratingAudio}
-              >
-                {isGeneratingAudio ? 'Loading...' : 'Play'}
-              </button>
-              )}
+            <div className="text-left">
+              {message.content.split('. ').map((part, index) => {
+                if (part.startsWith('The correct answer was:')) {
+                  return (
+                    <React.Fragment key={index}>
+                      <p className="font-bold">{part}</p>
+                      <p>&nbsp;</p> {/* This adds a line break */}
+                    </React.Fragment>
+                  )
+                } else if (part.startsWith('Your answer was:')) {
+                  return (
+                    <React.Fragment key={index}>
+                      <p>{part}</p>
+                      <p>&nbsp;</p> {/* This adds a line break */}
+                    </React.Fragment>
+                  )
+                } else {
+                  return <p key={index}>{part}</p>
+                }
+              })}
             </div>
-            {isUser && (
-              <img
-                src="https://www.teamsmart.ai/next-assets/profile-image.png"
-                className="w-9 h-9 rounded-full cursor-pointer"
-                alt="avatar"
-              />
+            {!isUser && (
+              audioUrl ? (
+                <audio controls className="self-end mt-2">
+                  <source src={audioUrl} type="audio/mpeg" />
+                  Your browser does not support the audio element.
+                </audio>
+              ) : (
+                <button 
+                  onClick={handleGenerateAudio} 
+                  className="self-end mt-2 bg-00 text-white font-bold py-2 px-4 rounded shadow active:shadow-none"
+                  disabled={isGeneratingAudio}
+                >
+                  {isGeneratingAudio ? 'Loading...' : 'Generate'}
+                </button>
+              )
             )}
           </div>
-        )
-      })}
-      {isLoadingAnswer && (
-        <div className="flex justify-start mb-4">
-          <img
-            src="img/tutor-avatar.png"
-            className="w-9 h-9 rounded-full"
-            alt="avatar"
-          />
-          <div className="bouncing-loader ml-2 p-2.5 px-4 bg-gray-200 dark:bg-gray-800 rounded-full space-x-1.5 flex justify-between items-center relative">
-            <span className="block w-3 h-3 rounded-full"></span>
-            <span className="block w-3 h-3 rounded-full"></span>
-            <span className="block w-3 h-3 rounded-full"></span>
-          </div>
+          {isUser && (
+            <img
+              src="https://www.teamsmart.ai/next-assets/profile-image.png"
+              className="w-9 h-9 rounded-full cursor-pointer"
+              alt="avatar"
+            />
+          )}
         </div>
-      )}
-      <div ref={messagesEndRef} />
-    </div>
+      )
+    })}
+    {isLoadingAnswer && (
+      <div className="flex justify-start mb-4">
+        <img
+          src="img/tutor-avatar.png"
+          className="w-9 h-9 rounded-full"
+          alt="avatar"
+        />
+                <div className="bouncing-loader ml-2 p-2.5 px-4 bg-gray-200 dark:bg-gray-800 rounded-full space-x-1.5 flex justify-between items-center relative">
+          <span className="block w-3 h-3 rounded-full"></span>
+          <span className="block w-3 h-3 rounded-full"></span>
+          <span className="block w-3 h-3 rounded-full"></span>
+        </div>
+      </div>
+    )}
+    <div ref={messagesEndRef} />
+  </div>
   )
 }
 
