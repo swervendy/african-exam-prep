cc89f28155c4ca34b6d39b92da9931677fcb495d
Aaron Cohen <aaronroicohen@gmail.com>
2023-10-17 13:47:37 +0300

    updated favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,6 +6,7 @@ class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700&display=swap" rel="stylesheet" />
+          <link rel="icon" href="/img/favicon.svg" />
         </Head>
         <body>
           <Main />
